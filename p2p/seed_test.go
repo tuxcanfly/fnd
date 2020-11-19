83223b3d06ba9f87138adb320be5117ae2ba3185
Javed Khan <tuxcanfly@gmail.com>
2020-11-19 12:38:05 +0530

    p2p: fix seed test

--- p2p/seed_test.go
+++ p2p/seed_test.go
@@ -1,15 +1,18 @@
 package p2p
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestLookupDNSSeeds(t *testing.T) {
 	seeds, err := ResolveDNSSeeds("seeds-test.ddrp.network")
 	require.NoError(t, err)
 
-	require.Equal(t, 2, len(seeds))
-	require.Contains(t, seeds, "10.1.0.1")
-	require.Contains(t, seeds, "10.1.0.2")
+	require.Equal(t, 4, len(seeds))
+	require.Contains(t, seeds, "198.54.117.200")
+	require.Contains(t, seeds, "198.54.117.199")
+	require.Contains(t, seeds, "198.54.117.198")
+	require.Contains(t, seeds, "198.54.117.197")
 }
